ef59385c4838f85313565c69493b945bb23ffdda
diogodiniz11 <diogogd@al.insper.edu.br>
2025-03-12 11:25:25 -0300

    ex3

--- exe3/main.c
+++ exe3/main.c
@@ -5,16 +5,22 @@
 const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
-volatile int flag_f_r = 0;
+volatile int flag_f_r = 0;  
+volatile bool estado_led = false; 
+alarm_id_t long_press_alarm = -1;  
+
+int64_t long_press_callback(alarm_id_t id, void *user_data) {
+    flag_f_r = 1;  
+    return 0;  e
+}
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        if (gpio == BTN_PIN_R)
-            flag_f_r = 1;
-
-    } else if (events == 0x8) { // rise edge
-        if (gpio == BTN_PIN_R)
-            flag_f_r = 0;
+    if (events == GPIO_IRQ_EDGE_FALL) {
+        long_press_alarm = add_alarm_in_ms(500, long_press_callback, NULL, false);
+    } else if (events == GPIO_IRQ_EDGE_RISE) {
+        if (long_press_alarm >= 0) {
+            cancel_alarm(long_press_alarm);
+        }
     }
 }
 
@@ -23,17 +29,20 @@ int main() {
 
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_put(LED_PIN_R, 0);  
 
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
-    gpio_pull_up(BTN_PIN_R);
+    gpio_pull_up(BTN_PIN_R);  
 
-    gpio_set_irq_enabled_with_callback(
-        BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
 
     while (true) {
-
         if (flag_f_r) {
+            flag_f_r = 0; 
+
+            estado_led = !estado_led;
+            gpio_put(LED_PIN_R, estado_led);  
         }
     }
 }
